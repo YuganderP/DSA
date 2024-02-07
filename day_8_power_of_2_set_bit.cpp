1d7e502ecb45b85eab6b565f215cc9b369610bc8
yugander <yuganderpilli@outlook.com>
2024-02-07 23:52:17 +0530

    power of 2 set bit commit

---
+++ day_8_power_of_2_set_bit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=9;
+    int set=0;
+    while(a!=0){
+        int bit=a&1;
+        
+        if(bit==1){
+            set+=1;
+        }
+       a=a>>1;
+    }
+    cout<<set;
+    if(set==1){
+        cout<<endl<<"The giver number is a power of 2";
+    }
+    
+}
