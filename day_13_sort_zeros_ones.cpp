d9b53c5a8488938e7eb79758ef4b896f4cea0e3c
yugander <yuganderpilli@outlook.com>
2024-02-13 00:28:04 +0530

    third commint

---
+++ day_13_sort_zeros_ones.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size=5;
+    int arr[size]={0,1,0,1,1};
+    int left=0;
+    int right=size-1;
+    while(left<right){
+        if(arr[left]==0){
+            left++;
+           
+        }else if(arr[right]==1){
+            right--;
+        }else if(arr[left]==1){
+            swap(arr[left],arr[right]);
+            left++;
+            right--;
+        }
+    }
+for(int i=0;i<size;i++){
+    cout<<arr[i]<<" ";
+}
+}
